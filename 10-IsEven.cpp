383ee78d1b2c3938990daddecb6e461e9d9c84e8
RCTM81 <michael_308@hotmail.es>
2025-11-21 17:16:24 +0000

    Add files via upload

---
+++ 10-IsEven.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+string checkEven(int num) {
+    if (num % 2 == 0) {
+        return "The provided number is even";
+    } 
+    else {
+        return "The provided number is odd";
+    }
+    
+}
+
+int main() {
+    int userNumber;
+
+    cout << "Please enter a number: ";
+    cin >> userNumber;
+
+    string result = checkEven(userNumber);
+    cout << result << endl;
+
+
+    return 0;
+}
